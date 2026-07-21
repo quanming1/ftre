5c52c3b37cbfee293b7c32cea3b5256a9e415b8f
quanming1 <2991537373@qq.com>
2026-07-21 17:43:43 +0800

    refactor: 删除废弃的 Pipeline 类

--- src/ftre/utils/__init__.py
+++ src/ftre/utils/__init__.py
@@ -1,4 +1,3 @@
-from .pipeline import Pipeline, Handler
 from .image_store import save_image, load_as_data_url
 
-__all__ = ["Pipeline", "Handler", "save_image", "load_as_data_url"]
+__all__ = ["save_image", "load_as_data_url"]
